9c4585778d91474bf2387a513e8d694f2507e1ee
matheuslazaroCCUFG <matheuslazaroccufg@gmail.com>
2021-06-12 18:20:32 -0300

    new file:   cmake-build-debug/CMakeCache.txt        new file:   cmake-build-debug/CMakeFiles/3.19.2/CMakeCCompiler.cmake        new file:   cmake-build-debug/CMakeFiles/3.19.2/CMakeDetermineCompilerA>        new file:   cmake-build-debug/CMakeFiles/3.19.2/CMakeSystem.cmake        new file:   cmake-build-debug/CMakeFiles/3.19.2/CompilerIdC/CMakeCCompi>        new file:   cmake-build-debug/CMakeFiles/3.19.2/CompilerIdC/a.out        new file:   cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake        new file:   cmake-build-debug/CMakeFiles/CMakeOutput.log        new file:   cmake-build-debug/CMakeFiles/Makefile.cmake        new file:   cmake-build-debug/CMakeFiles/Makefile2        new file:   cmake-build-debug/CMakeFiles/TargetDirectories.txt        new file:   cmake-build-debug/CMakeFiles/clion-environment.txt

---
+++ cmake-build-debug/CMakeFiles/codewars_C.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codewars_C.dir/codewars-C/7kyu/growthOfAPopulation/growthOfAPopulation.c.o"
+  "CMakeFiles/codewars_C.dir/codewars-C/7kyu/vowelCount/get_count.c.o"
+  "codewars_C"
+  "codewars_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/codewars_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
